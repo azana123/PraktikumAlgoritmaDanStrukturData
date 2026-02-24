55f5ccfde2b6a9abecb1c478b3bd55eec3cf8672
azana123 <azanalutfi3212gmail.com>
2026-02-24 08:32:28 +0700

    Percobaan 1

---
+++ jobsheet3/Mahasiswa09.java
@@ -0,0 +1,8 @@
+package PraktikumAlgoritmaDanStrukturData.jobsheet3;
+
+public class Mahasiswa09 {
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+}
