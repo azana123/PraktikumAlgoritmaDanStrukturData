7e7e2e11fce3ef5a050e6fb3cbfad7f40e766e82
azana123 <azanalutfi321@gmail.com>
2026-05-19 09:40:59 +0700

    CM_kuis

---
+++ CM_Kuis/Pembeli.java
@@ -0,0 +1,11 @@
+package CM_Kuis;
+
+public class Pembeli {
+    String namaPembeli;
+    String noHp;
+
+    Pembeli(String namaPembeli, String noHp) {
+        this.namaPembeli = namaPembeli;
+        this.noHp = noHp;
+    }
+}
---
+++ CM_Kuis/Pesanan.java
@@ -0,0 +1,13 @@
+package CM_Kuis;
+
+public class Pesanan {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+    
+    Pesanan(int kodePesanan, String namaPesanan, int harga) {
+        this.kodePesanan = kodePesanan;
+        this.namaPesanan = namaPesanan;
+        this.harga = harga;
+    }
+}
